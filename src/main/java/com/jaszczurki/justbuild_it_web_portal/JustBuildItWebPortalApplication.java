2266d81dc6f5b8bcc363d2fcaf0a6fada77d2f2d
Konrad Pisarski <kpisar754@gmail.com>
2023-05-23 19:42:58 +0200

    jBuild init commit + app.properties/docker-compose file

---
+++ src/main/java/com/jaszczurki/justbuild_it_web_portal/JustBuildItWebPortalApplication.java
@@ -0,0 +1,13 @@
+package com.jaszczurki.justbuild_it_web_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustBuildItWebPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustBuildItWebPortalApplication.class, args);
+    }
+
+}
